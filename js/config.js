6ef523b77fa8828f43ac6831e79ca7172f679850
fcarsi-droid <fcarsi@gmail.com>
2025-10-17 19:48:42 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 
 window.SUPABASE = {
-  url: 'https://SEU_PROJETO.supabase.co',
-  anonKey: 'SUA_CHAVE_PUBLICA'
+  url: 'https://thndwyyjlkflaenalvbc.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRobmR3eXlqbGtmbGFlbmFsdmJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3MzcxMzMsImV4cCI6MjA3NjMxMzEzM30.XiksMdgx98N2zJ95vbI_1P9TT8nlxoYEG1cBvc_ML80'
 };
